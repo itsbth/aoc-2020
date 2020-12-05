b03f9a4cd6196034beb265297e9fd32484a95078
Bjørn Tore Håvie <itsbth@itsbth.com>
2020-12-05 13:14:18 +0100

    d5: run shellcheck -o all and shfmt

--- d5/solve.sh
+++ d5/solve.sh
@@ -2,10 +2,10 @@
 
 set -euf -o pipefail
 
-ids="$( (echo "ibase=2;" && < "${1:-sample}" tr 'FLBR' '0011' | sort) | bc )"
-min=$(echo "$ids" | head -n1)
-max=$(echo "$ids" | tail -n1)
+ids="$( (echo "ibase=2;" && tr <"${1:-sample}" 'FLBR' '0011' | sort) | bc)"
+min=$(echo "${ids}" | head -n1)
+max=$(echo "${ids}" | tail -n1)
 
-echo "part 1: $max"
+echo "part 1: ${max}"
 echo "part 2:"
-diff <(seq "$min" "$max") <(echo "$ids")
+diff <(seq "${min}" "${max}") <(echo "${ids}")
